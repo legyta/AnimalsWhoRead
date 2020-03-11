6b5a227f82b45b5025274367999e6bc2fd0b4b69
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-03-11 12:03:47 +0100

    fun and contact responsive

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -20,73 +20,142 @@ const Fun = props => (
     <header className="header-fun">
       <h1>Animals have Fun !</h1>
     </header>
-
-    <div className="fun-animals">
-      {" "}
-      <p>Here are some animals enjoying their time!</p>
-    </div>
-
     <div className="all-arrows">
       <img src={Arrow} alt="arrow" className="arrow-fun" />
       <img src={Arrow} alt="arrow" className="arrow-fun" />
       <img src={Arrow} alt="arrow" className="arrow-fun" />
     </div>
-    {/* <div className="frame--fun"> */}
-    <div className="gif">
+    <div className="fun-animals">
+      {" "}
       {/* <div className="frame--fun"> */}
-      <div className="row">
-        <div className="frame--fun">
-          <div className="col-md-4">
-            {" "}
-            <img src={giphy} alt="gif" height="350" width="350" />{" "}
+      <div className="frame--fun">
+        <p>Here are some animals enjoying their time!</p>
+        <div className="gif">
+          {/* <div className="frame--fun"> */}
+          <div className="row">
+            <div className="col-md-4">
+              {" "}
+              <img
+                src={giphy}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />{" "}
+            </div>
+            <div className="col-md-4">
+              <img
+                src={pauk}
+                alt="animal"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              {" "}
+              <img
+                src={animal2}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
           </div>
-          <div className="col-md-4">
-            <img src={pauk} alt="animal" height="350" width="350" />
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={animal3}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={animal4}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              {" "}
+              <img
+                src={cat}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
           </div>
-          <div className="col-md-4">
-            {" "}
-            <img src={animal2} alt="gif" height="350" width="350" />
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={dog}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              {" "}
+              <img
+                src={octop}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={dog2}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <img src={animal3} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-md-4">
-            <img src={animal4} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img src={cat} alt="gif" height="350" width="350" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <img src={dog} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img src={octop} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-md-4">
-            <img src={dog2} alt="gif" height="350" width="350" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <img src={dog3} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-md-4">
-            <img src={elef} alt="gif" height="350" width="350" />
-          </div>
-          <div className="col-sm-4">
-            {" "}
-            <img src={pig} alt="gif" height="350" width="350" />
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={dog3}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={elef}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={pig}
+                alt="gif"
+                className="gifs"
+                height="350"
+                width="350"
+              />
+            </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
     </div>
-    {/* </div> */}
     <div className="button-fun">
       <button className="active-article__button">
         <Link to="/">Go Home</Link>
