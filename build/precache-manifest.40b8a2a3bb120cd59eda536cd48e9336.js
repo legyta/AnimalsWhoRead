bfbbc1af456a2b16acb7f37603ac2f4f36d46d3c
Ligita <legyta@gmail.com>
2020-03-13 18:42:35 +0100

    Added build

---
+++ build/precache-manifest.40b8a2a3bb120cd59eda536cd48e9336.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c7c88325d7dc4ed8b94ef552b4df5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d8f754faf0aed16ae0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fc9ddbfa45af8165543b",
+    "url": "/static/css/main.87153eae.chunk.css"
+  },
+  {
+    "revision": "14d8f754faf0aed16ae0",
+    "url": "/static/js/2.ce6c0a50.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ce6c0a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9ddbfa45af8165543b",
+    "url": "/static/js/main.6d737297.chunk.js"
+  },
+  {
+    "revision": "e790d7d4e7b7b56b9beb",
+    "url": "/static/js/runtime-main.4d25431e.js"
+  },
+  {
+    "revision": "478526b7946a511a89e1ce99f8ce7256",
+    "url": "/static/media/Arrow.478526b7.svg"
+  },
+  {
+    "revision": "0561245cb899f7737360ba640841bff9",
+    "url": "/static/media/Panda.0561245c.svg"
+  },
+  {
+    "revision": "b2282efe273eeafd7d243a190118b43f",
+    "url": "/static/media/Paw.b2282efe.svg"
+  },
+  {
+    "revision": "c41c6e80d803edfd91bb577b12a78d73",
+    "url": "/static/media/Paw.c41c6e80.svg"
+  },
+  {
+    "revision": "50b370c02d995df1ffcf015b95872db3",
+    "url": "/static/media/animal2.50b370c0.gif"
+  },
+  {
+    "revision": "9d2bf483aedbcb2d353d883aa7f4374a",
+    "url": "/static/media/animal3.9d2bf483.gif"
+  },
+  {
+    "revision": "6cf2ceb7de00d801532d9a0f88cb66f8",
+    "url": "/static/media/animal4.6cf2ceb7.gif"
+  },
+  {
+    "revision": "47055ec836c726009b5925b414642f2f",
+    "url": "/static/media/cat.47055ec8.gif"
+  },
+  {
+    "revision": "65301357188de9dcd5900ab6d66f1bdb",
+    "url": "/static/media/dog.65301357.gif"
+  },
+  {
+    "revision": "08a038b931e828e956021b8aae0ffb8a",
+    "url": "/static/media/dog2.08a038b9.gif"
+  },
+  {
+    "revision": "b8cf0757ccbb2f2d829b09ea9138cc98",
+    "url": "/static/media/dog3.b8cf0757.gif"
+  },
+  {
+    "revision": "29e98e795746770b2d944be35d701756",
+    "url": "/static/media/elef.29e98e79.gif"
+  },
+  {
+    "revision": "f293373f9f1698cd4db6075ebfbc6b3f",
+    "url": "/static/media/frame.f293373f.svg"
+  },
+  {
+    "revision": "b5d2ac57d924de62b285fce25c5b5e96",
+    "url": "/static/media/giphy.b5d2ac57.gif"
+  },
+  {
+    "revision": "08a63e6bbbbf10d0376056ce48f28333",
+    "url": "/static/media/octop.08a63e6b.gif"
+  },
+  {
+    "revision": "bd16c15801a7419d7cda3a6c02199c25",
+    "url": "/static/media/pauk.bd16c158.gif"
+  },
+  {
+    "revision": "a66a247c5456b39cd82109288faa04db",
+    "url": "/static/media/pig.a66a247c.gif"
+  }
+]);
