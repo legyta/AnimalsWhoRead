4b82bc10d38577734f6bd16cb088d768d2e4d754
Ligita <legyta@gmail.com>
2020-03-14 17:12:53 +0100

    fix style frame plus article searchr

--- src/App.js
+++ src/App.js
@@ -9,44 +9,27 @@ import Fun from "./components/Fun";
 import IndexPage from "./components/IndexPage";
 
 class App extends Component {
-  // state = {
-  //   sideDrawerOpen: false,
-  //   articles: []
-  // };
+  state = {
+    articles: []
+  };
 
-  // drawerToggleClickHandler = () => {
-  //   this.setState(prevState => {
-  //     return { sideDrawerOpen: !prevState.sideDrawerOpen };
-  //   });
-  // };
+  getArticles = async e => {
+    const articleName = e.target.elements.articleName.value || "chicken";
+    e.preventDefault();
+    const api_call = await fetch(
+      `http://newsapi.org/v2/everything?q=${articleName}&apiKey=${API_KEY}&pageSize=20`
+    );
 
-  // backdropClickHandler = () => {
-  //   this.setState({ sideDrawerOpen: false });
-  // };
+    const data = await api_call.json();
+    this.setState({ articles: data.articles });
+  };
 
-  // getArticles = async e => {
-  //   const articleName = e.target.elements.articleName.value || "chicken";
-  //   e.preventDefault();
-  //   const api_call = await fetch(
-  //     `http://newsapi.org/v2/everything?q=${articleName}&apiKey=${API_KEY}&pageSize=20`
-  //   );
-
-  //   const data = await api_call.json();
-  //   this.setState({ articles: data.articles });
-  // };
-
-  // componentDidUpdate = () => {
-  //   const articles = JSON.stringify(this.state.articles);
-  //   localStorage.setItem("articles", articles);
-  // };
+  componentDidUpdate = () => {
+    const articles = JSON.stringify(this.state.articles);
+    localStorage.setItem("articles", articles);
+  };
 
   render() {
-    // let backdrop;
-
-    // if (this.state.sideDrawerOpen) {
-    //   backdrop = <BackDrop click={this.backdropClickHandler} />;
-    // }
-
     const App = () => (
       <div className="App" style={{ height: "100%" }}>
         <Switch>
