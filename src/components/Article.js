3c660aca2c89d050108c776dc2b4535dbdd3be8b
Ligita <legyta@gmail.com>
2020-03-14 18:59:25 +0100

    article page show

--- src/components/Article.js
+++ src/components/Article.js
@@ -24,7 +24,12 @@ class Article extends React.Component {
     const article = this.state.activeArticle;
     return (
       <div className="container--article" id="article">
-        <Link to="/article"></Link>
+        <Link
+          to={{
+            pathname: `/article/${article.title}`,
+            state: { article: article.title }
+          }}
+        ></Link>
         <NavBar />
         <div className="article--wrap">
           {this.state.activeArticle.length !== 0 && (
